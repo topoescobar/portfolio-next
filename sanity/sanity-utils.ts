4b6900c3bc6df9464f459aaf27b59e420a2a2c1a
topoescobar <ivan90ge@gmail.com>
2023-05-15 15:51:55 -0300

    maqueta proyectos

--- sanity/sanity-utils.ts
+++ sanity/sanity-utils.ts
@@ -1,7 +1,8 @@
 import { createClient, groq } from "next-sanity";
 
 //leer los datos cargados en sanity studio
-export async function getProjects() {
+// export async function getProjects(): Promise<Project[]> { //de esta forma no se tiene que declarar el type en cada componente que lo use
+  export async function getProjects() {
   const client = createClient({
     projectId: '69y3yni7',
     dataset: 'production',
